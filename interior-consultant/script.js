5ed1a211b53bc7a2d93cb76d6c27cc754e9081e6
Pankaj Das <70905358+Pankaj455@users.noreply.github.com>
2021-10-02 23:52:23 +0530

    Delete interior-consultant directory

--- interior-consultant/script.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector(".burger")
-const nav = document.querySelector("nav")
-burger.addEventListener("click", () => {
-    nav.classList.toggle("show")
-    document.body.style.overflowY = nav.classList.contains("show") ? "hidden" : "visible"
-})
